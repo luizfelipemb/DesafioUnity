614fca1e97ac0e81ec7339f4eb2c534fe1317d11
Luiz Felipe <57150454+luizfelipemb@users.noreply.github.com>
2021-12-26 00:47:53 +0000

    square scenebonus

---
+++ DesafioUnity/Assets/Scripts/BonusScene/SceneBonusManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneBonusManager : MonoBehaviour
+{
+    [SerializeField] private List<Square> squareList;
+    [SerializeField] private Transform redBegin;
+    [SerializeField] private Transform greenBegin;
+    [SerializeField] private Transform blueBegin;
+    private int _redSquares, _greenSquares, _blueSquares;
+    
+    public IEnumerator StartSorting()
+    {
+        foreach (var square in squareList)
+        {
+            Debug.Log(square.name);
+            yield return new WaitForSeconds(0.5f);
+            switch (square.color)
+            {
+                case SquareColor.red:
+                    Vector3 target = redBegin.position;
+                    target.x += _redSquares * 2;
+                    square.MoveTo(target);
+                    _redSquares++;
+                    break;
+                case SquareColor.green:
+                    
+                    square.MoveTo(greenBegin.position);
+                    _greenSquares++;
+                    break;
+                case SquareColor.blue:
+                    
+                    square.MoveTo(blueBegin.position);
+                    _blueSquares++;
+                    break;
+            }
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartSorting());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ DesafioUnity/Assets/Scripts/BonusScene/Square.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public enum SquareColor{red,green,blue}
+public class Square : MonoBehaviour
+{
+    public SquareColor color;
+    public float StoppingDistance;
+    public float Speed;
+    private SpriteRenderer _spriteRenderer;
+    private Vector3 _target;
+    private bool _haveToMove = false;
+    private Vector3 _targetDirection;
+    void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        NewColor();
+    }
+
+    private void NewColor()
+    {
+        color = (SquareColor) Random.Range(0, System.Enum.GetValues(typeof(SquareColor)).Length);
+        switch(color)
+        {
+            case SquareColor.red:
+                _spriteRenderer.color = Color.red;
+                break;
+            case SquareColor.green:
+                _spriteRenderer.color = Color.green;
+                break;
+            case SquareColor.blue:
+                _spriteRenderer.color = Color.blue;
+                break;
+        }
+    }
+
+    public void MoveTo(Vector3 target)
+    {
+        _haveToMove = true;
+        _target = target;
+        _targetDirection = _target - transform.position;
+        _targetDirection = _targetDirection.normalized;
+    }
+
+    private void Update()
+    {
+        if (_haveToMove)
+        {
+            if (Vector3.Distance(transform.position, _target) < StoppingDistance)
+            {
+                _haveToMove = false;
+            }
+            transform.position += _targetDirection * (Speed * Time.deltaTime);
+        }   
+    }
+}
